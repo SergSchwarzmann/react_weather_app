d09114104ad8f3253ea2da91c2c9eca0f9ccd3fd
InsKill <tm_adm@mail.ru>
2023-05-23 15:45:45 +0600

    test 2

--- src/components/Cities.tsx
+++ src/components/Cities.tsx
@@ -6,16 +6,17 @@ import "../styles/Cities.css";
 import City from './City';
 
 function Cities(props) {
+    let [coords, setCoords] = React.useState();
 
-    const askWeather = (lat, lon) => {
-        City(lat, lon)
+    const askWeather = (coords) => {
+        setCoords(coords)
     }
 
     return (
         <div className="cities">
             This is cities
             <ul>
-                <li><button onClick={() => askWeather(50, 70)}>Temirtau</button></li>
+                <li><button onClick={() => askWeather([50, 70])}>Temirtau</button></li>
                 <li><button>Astana</button></li>
                 <li><button>Almaty</button></li>
                 <li><button>Moscow</button></li>
